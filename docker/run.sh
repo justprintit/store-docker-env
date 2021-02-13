7feeed64e161c41704a33da440cae67eac7dfab5
Alejandro Mery <amery@geeks.cl>
2021-02-13 21:26:20 +0000

    docker: expose GODOC_PORT and SERVER_PORT as defined by config.mk
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- docker/run.sh
+++ docker/run.sh
@@ -5,4 +5,10 @@ set -eu
 export DOCKER_RUN_WS="$(dirname "$0")"
 export DOCKER_DIR="$(dirname "$(readlink -f "$0")")"
 
+. "$DOCKER_RUN_WS/config.mk"
+
+for x in ${SERVER_PORT:-} ${GODOC_PORT:-}; do
+	export DOCKER_EXTRA_OPTS="${DOCKER_EXTRA_OPTS:+$DOCKER_EXTRA_OPTS }-p $x:$x/tcp"
+done
+
 exec docker-builder-run "$@"
