4fa84981ba5d14817ebd35bca2dda5ea6d404077
Alejandro Mery <amery@geeks.cl>
2021-02-15 14:18:49 +0000

    docker: add `DOCKER_EXPOSE` handling
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- README.md
+++ README.md
@@ -31,3 +31,19 @@ or executing via `x`
 ## Customizing
 
 Key variables are defined by `config.mk` which shall remain sh and `make` compatible
+
+## Exposing Ports
+
+By default `x` will expose `SERVER_PORT` and `GODOC_PORT` as defined by `config.mk`,
+or falling back to 8080 and 9090 correspondingly if undefined.
+
+To prevent `x` from exposing any port, export an empty `DOCKER_EXPOSE` variable
+```
+$ export DOCKER_EXPOSE=
+$ x
+```
+
+There are also two special cases if `DOCKER_EXPOSE` hasn't been disabled.
+when calling `x make doc` it will only expose `GODOC_PORT`, or 9090/tcp if undefined,
+and when calling `x make dev` or `x make run` it will expose `SERVER_PORT`, or 8080/tcp if undefined,
+and every other extra port specified via `DOCKER_EXPOSE`.
--- docker/run.sh
+++ docker/run.sh
@@ -5,10 +5,33 @@ set -eu
 export DOCKER_RUN_WS="$(dirname "$0")"
 export DOCKER_DIR="$(dirname "$(readlink -f "$0")")"
 
-. "$DOCKER_RUN_WS/config.mk"
+# don't expose anything if DOCKER_EXPOSE is defined empty
+if ! set | grep -q "^DOCKER_EXPOSE=\(''\)\?$"; then
+	. "$DOCKER_RUN_WS/config.mk"
 
-for x in ${SERVER_PORT:-} ${GODOC_PORT:-}; do
-	export DOCKER_EXTRA_OPTS="${DOCKER_EXTRA_OPTS:+$DOCKER_EXTRA_OPTS }-p $x:$x/tcp"
-done
+	: ${GODOC_PORT:=9090}
+	: ${SERVER_PORT:=8080}
+
+	# special cases
+	#
+	case "${1:-} ${2:-}" in
+	"make doc")
+		DOCKER_EXPOSE="$GODOC_PORT"
+		;;
+	"make run"|"make dev")
+		DOCKER_EXPOSE="${DOCKER_EXPOSE:+$DOCKER_EXPOSE }$SERVER_PORT"
+		;;
+	*)
+		DOCKER_EXPOSE="${DOCKER_EXPOSE:+$DOCKER_EXPOSE }$SERVER_PORT $GODOC_PORT"
+		;;
+	esac
+
+	for x in $DOCKER_EXPOSE; do
+		export DOCKER_EXTRA_OPTS="${DOCKER_EXTRA_OPTS:+$DOCKER_EXTRA_OPTS }-p $x:$x/tcp"
+	done
+fi
+
+unset DOCKER_EXPOSE
+export -n DOCKER_EXPOSE
 
 exec docker-builder-run "$@"
